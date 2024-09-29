9578717260bfac2c644988b9e34a5120abeda271
sciencenoodles <181941403+sciencenoodles@users.noreply.github.com>
2024-09-29 13:37:36 +0800

    Submit solutions for questions 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+    const min = Math.min(num1, num2);
+    const max = Math.max(num1, num2);
+    let sum = 0;
+    for (let i = min; i<= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
